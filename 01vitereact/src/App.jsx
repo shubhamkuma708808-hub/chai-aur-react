9265467dfecdcee9fb3558b25bf93abe4f832711
shubhamkuma708808-hub <shubhamkuma708808@gmail.com>
2026-07-26 10:06:26 +0000

    react project react and vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1> chia aur react with vite | Shubham </h1>
+  )
+}
+
+export default App
---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1> chai aur react with react  | Shubham </h1>
+  );
+}
+
+export default App;
