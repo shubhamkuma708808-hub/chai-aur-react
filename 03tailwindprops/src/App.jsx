d13cc6e4fab6a5f8724c6c35f943e93ed19a7c6a
shubhamkuma708808-hub <shubhamkuma708808@gmail.com>
2026-08-05 06:51:10 +0000

    tailwind & props min project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './componets/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "shubham",
+    age: 21
+  }
+  let newArr = [1,2,3]
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl'>tailwind test</h1>
+    <Card username="chaiaurcode"/>
+
+{/* <div class="flex flex-col items-center gap-6 p-7 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="/public/book10.jpg" />
+  </div>
+  <div class="flex items-center">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div> */}
+
+  
+
+    </>
+    
+  )
+}
+
+export default App
