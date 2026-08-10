ddf4489ecc0973eeec59243e1f18254c2ff7c459
shubhamkuma708808-hub <shubhamkuma708808@gmail.com>
2026-08-10 18:22:09 +0000

    React router crash course

---
+++ myreactrouter/src/components/Github/Github.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { data } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+
+function Github() {
+    const data = useLoaderData()
+    //const [dta, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+
+    // }, [])
+    return(
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+            <img src={ data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const respones = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return respones.json()
+}
